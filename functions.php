086f9ea5438884f5d7cf972c707b7e63710e08a3
Randolfh <ferarizarandolfh.ifliba@gmail.com>
2020-12-25 15:30:59 +0800

    added version parameter

--- functions.php
+++ functions.php
@@ -1,12 +1,14 @@
 <!-- all my functions here -->
 
 <?php
+//variable version
+    $version = wp_get_theme() -> get('Version');
 
     function nostagla_register_styles(){
 
-            wp_enqueue_style('nostagla-style', get_template_directory_uri() . "/style.css", array('nostagla-bootstrap'), '1.0','all'); //right path for stylesheet
-            wp_enqueue_style('nostagla-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '1.0','all'); //bootstrap
-            wp_enqueue_style('nostagla-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '1.0','all'); //fontawesome
+            wp_enqueue_style('nostagla-style', get_template_directory_uri() . "/style.css", array('nostagla-bootstrap'), $version,'all'); //right path for stylesheet
+            wp_enqueue_style('nostagla-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1','all'); //bootstrap
+            wp_enqueue_style('nostagla-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0','all'); //fontawesome
 
     }
 
