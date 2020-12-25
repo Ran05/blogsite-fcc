b879ce47742f42a0abb2446c641c96a91a0282a3
Randolfh <ferarizarandolfh.ifliba@gmail.com>
2020-12-25 15:18:17 +0800

    enqueue style-bt-fontawesome

--- functions.php
+++ functions.php
@@ -4,7 +4,9 @@
 
     function nostagla_register_styles(){
 
-            wp_enqueue_style('nostagla-bootstrap', get_template_directory_uri() . "/style.css", array(), '1.0','all');
+            wp_enqueue_style('nostagla-style', get_template_directory_uri() . "/style.css", array(), '1.0','all'); //right path for stylesheet
+            wp_enqueue_style('nostagla-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '1.0','all'); //bootstrap
+            wp_enqueue_style('nostagla-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '1.0','all'); //fontawesome
 
     }
 
