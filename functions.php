288eafb8ebc210ceb28288dfca8db639c5c8be6c
Randolfh <ferarizarandolfh.ifliba@gmail.com>
2020-12-25 16:10:40 +0800

    path for js

--- functions.php
+++ functions.php
@@ -11,6 +11,17 @@
             wp_enqueue_style('nostagla-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0','all'); //fontawesome
 
     }
-
-    add_action('wp_enqueue_scripts', 'nostagla_register_styles');
+    
+    //wordpress hooks
+    add_action('wp_enqueue_scripts', 'nostagla_register_styles'); 
+    
+    
+    function nostagla_register_scripts(){
+            wp_enqueue_scripts('nostagla-jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', array(),'3.4.1',true);
+            wp_enqueue_scripts('nostagla-popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(),'1.16.0',true);
+            wp_enqueue_scripts('nostagla-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(),'4.4.1',true);
+            wp_enqueue_scripts('nostagla-jquery', get_template_directory_uri()."/assets/js/main.js", array(),'1.0',true);
+    }
+    //wp hooks for scripts
+    add_action('wp_enqueue_scripts', 'nostagla_register_scripts'); 
 ?>
